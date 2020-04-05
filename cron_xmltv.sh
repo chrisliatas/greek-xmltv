e6ff04e9b742033391c8cb3bb721f3121e9e971a
chrisliatas <chris@liatas.com>
2020-04-05 21:05:47 +0300

    modified/improved bash scripts.

--- cron_xmltv.sh
+++ cron_xmltv.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Run this with `sudo` to install a new cron for root that runs the daily job for greek-xmltv.
 
-command="/bin/bash /home/$USER/greek-xmltv/getxmltv.sh"
-job="0 45 5 ? * * * $command"
+appdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+command="/bin/bash ${appdir}/getxmltv.sh >> /var/log/cron.log"
+job="45 5 * * * $command"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
--- getxmltv.sh
+++ getxmltv.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Run this script from the directory it is in with the Dockerfile and docker-compose.yml
 
-appdir="/home/$USER/greek-xmltv/"
-compose_file="${appdir}docker-compose.yml"
-export_dir="${appdir}scrapy_export/"
+timestamp() {
+  date '+%Y-%m-%d %H:%M:%S'
+}
+
+appdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+compose_file="${appdir}/docker-compose.yml"
+export_dir="${appdir}/scrapy_export/"
 target_dir="/mnt/storage/Downloads/xmltv/"
 
-/usr/local/bin/docker-compose -f "$compose_file" up -d & sleep 300; /usr/local/bin/docker-compose stop
+mkdir -p /mnt/storage/Downloads/xmltv
+echo "$(timestamp): getxmltv - Starting docker-compose services"
+/usr/local/bin/docker-compose -f "$compose_file" up -d && sleep 300; /usr/local/bin/docker-compose -f "$compose_file" stop
+echo "$(timestamp): getxmltv - Stopped docker-compose services"
 cp "${export_dir}"*.xml "${target_dir}"
 rm -r "${export_dir}"*.xml
-# remove json files older thatn 7 days.
+# remove json files older than 7 days.
 find "${export_dir}"*.json -mtime +7 -type f -delete
+echo "$(timestamp): getxmltv - Finished"
