11220f02c6cca39bcbbc78210edb4efd7423c8d4
chrisliatas <chris@liatas.com>
2020-04-24 18:44:47 +0300

    added push generated xmltv file to remote repo

--- getxmltv.sh
+++ getxmltv.sh
@@ -9,6 +9,8 @@ appdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 compose_file="${appdir}/docker-compose.yml"
 export_dir="${appdir}/scrapy_export"
 target_dir="/mnt/storage/Downloads/xmltv"
+generated_xmltv_dir="/generated_xmltv"
+xmltv_filename="xmltv_GREECE"
 
 echo "$(timestamp): getxmltv - Starting docker-compose services"
 echo ""
@@ -16,9 +18,15 @@ echo ""
 echo ""
 echo "$(timestamp): getxmltv - Stopped docker-compose services"
 [ ! -d "$target_dir" ] && mkdir -p "$target_dir" && chmod 777 "$target_dir"
+[ ! -d "$generated_xmltv_dir" ] && mkdir -p "$generated_xmltv_dir" && chmod 777 "$generated_xmltv_dir"
 cp "${export_dir}"/* "${target_dir}"/
+cp "${export_dir}/${xmltv_filename}.xml" "${generated_xmltv_dir}/"
 rm -f "${export_dir}"/*
-chmod 666 "$target_dir"/*
+chmod 666 "${target_dir}"/*
+chmod 666 "${generated_xmltv_dir}"/*
 # remove json files older than 7 days.
 find "${target_dir}"/*.json -mtime +7 -type f -delete
+# Commit new generated xmltv file and git push
+cd "$appdir" && /usr/bin/git add --all && /usr/bin/git commit -m "Auto generated xmltv file for $(timestamp)"
+cd "$appdir" && /usr/bin/git push origin master
 echo "$(timestamp): getxmltv - Finished"
